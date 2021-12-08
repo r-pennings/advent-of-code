1ac0983d5876970eea2e8ab0dbc8e180405c5c85
Roel Pennings <roelpennings@hotmail.com>
2021-12-09 00:13:46 +0100

    Update 4th day

--- 2021/dec-04/index.php
+++ 2021/dec-04/index.php
@@ -1,48 +1,148 @@
 <?php
 
-$input = file_get_contents("test.txt");
-$lines = explode("\r\n", $input);
+$input = file_get_contents("input.txt");
 
-// PART 1
-$tmp = $lines;
-$balls = array_shift($tmp);
+$input = trim($input);
+$input = str_replace("\r\n", " ", $input);
+$input = preg_replace("!\s+!", " ", $input);
+$inputArray = explode(" ", $input);
+
+$balls = array_shift($inputArray);
 $balls = explode(",", $balls);
 
-array_shift($tmp); // empty line
+$cards = array_chunk($inputArray, 25);
 
-$boards = [];
-$index = 0;
-foreach ($tmp as $line) {
-    $i = "board-" . $index;
-
-    $boards[$i] = [];
-
-    // Rows
-    $boards[$i]["rows"] = [];
-
-    if (!empty($line)) {
-        preg_match_all("/(\d+) *?(\d+) *?(\d+) *?(\d+) *?(\d+)/", $line, $matches);
-
-        $array = [];
-        foreach (range(1, count($matches) - 1) as $idx) {
-            $array[] = $matches[$idx][0];
-        }
-
-        $boards[$i]["rows"][] = $array;
-    } else {
-        $index++;
+// Functions
+function calculateWinner($markedCard, $gameArray): int
+{
+    $cardSum = 0;
+    foreach ($gameArray[$markedCard]["rows"] as $row) {
+        $cardSum += (int)array_sum($row);
     }
-
-    // Cols
-    $boards[$i]["cols"] = [];
+    return $cardSum;
 }
 
-var_dump($boards);
+function createGame($cards, $cardNr): array
+{
+    $game = array();
 
-//$boards
+    $cols = array();
+    foreach ($cards as $card) {
+        $rows = array_chunk($card, 5);
 
-echo "PART 1: ?";
+        for ($y = 0; $y < 5; $y++) {
+            for ($x = 0; $x < 5; $x++) {
+                $cols[$y][$x] = $card[($y + ($x * 5))];
+            }
+        }
+
+        $game[$cardNr]["rows"] = $rows;
+        $game[$cardNr]["cols"] = $cols;
+        $cardNr++;
+    }
+
+    return $game;
+}
+
+// PART 1
+$winners = array();
+$cardNr = 0;
+$game = createGame($cards, $cardNr);
+
+$ballIndex = 0;
+$result = null;
+while (!$result) {
+    $ball = $balls[$ballIndex];
+
+    $cardNr = 0;
+    foreach ($game as $card) {
+        if (!in_array($cardNr, $winners)) {
+            $rowNum = $colNum = 0;
+
+            // check rows
+            foreach ($card["rows"] as $row) {
+                $found = array_search($ball, $row);
+                if ($found !== false) {
+                    $cellsFound = count($game[$cardNr]["rows"][$rowNum]);
+                    unset($game[$cardNr]["rows"][$rowNum][$found]);
+                    $cellsLeft = count($game[$cardNr]["rows"][$rowNum]);
+
+                    if (!$cellsLeft) {
+                        $result = calculateWinner($cardNr, $game) * $ball;
+                        break;
+                    }
+                }
+                $rowNum++;
+            }
+
+            // check cols
+            foreach ($card["cols"] as $col) {
+                $found = array_search($ball, $col);
+                if ($found !== false) {
+                    $cellsFound = count($game[$cardNr]["cols"][$colNum]);
+                    unset($game[$cardNr]["cols"][$colNum][$found]);
+                    $cellsLeft = count($game[$cardNr]["cols"][$colNum]);
+                    if (!$cellsLeft) {
+                        $result = calculateWinner($cardNr, $game) * $ball;
+                        break;
+                    }
+                }
+                $colNum++;
+            }
+        }
+        $cardNr++;
+    }
+
+    $ballIndex++;
+}
+
+echo "PART 1: ".$result;
 
 // PART 2
+$winners = array();
+$cardNr = 0;
+$game = createGame($cards, $cardNr);
 
-echo "PART 2: ?";
+$result = null;
+foreach ($balls as $ball) {
+    $cardNr = 0;
+    foreach ($game as $card) {
+        if (!in_array($cardNr, $winners)) {
+            $rowNum = $colNum = 0;
+
+            // check rows
+            foreach ($card["rows"] as $row) {
+                $found = array_search($ball, $row);
+                if ($found !== false) {
+                    $cellsFound = count($game[$cardNr]["rows"][$rowNum]);
+                    unset($game[$cardNr]["rows"][$rowNum][$found]);
+                    $cellsLeft = count($game[$cardNr]["rows"][$rowNum]);
+
+                    if (!$cellsLeft) {
+                        $result = calculateWinner($cardNr, $game) * $ball;
+                        $winners[] = $cardNr;
+                    }
+                }
+                $rowNum++;
+            }
+
+            // check cols
+            foreach ($card["cols"] as $col) {
+                $found = array_search($ball, $col);
+                if ($found !== false) {
+                    $cellsFound = count($game[$cardNr]["cols"][$colNum]);
+                    unset($game[$cardNr]["cols"][$colNum][$found]);
+                    $cellsLeft = count($game[$cardNr]["cols"][$colNum]);
+                    if (!$cellsLeft) {
+                        $result = calculateWinner($cardNr, $game) * $ball;
+                        $winners[] = $cardNr;
+                    }
+                }
+                $colNum++;
+            }
+        }
+        $cardNr++;
+    }
+}
+
+echo "PART 2: ".$result;
